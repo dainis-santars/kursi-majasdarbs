0f302ffde10f8ead81e96c208d11a24156f3d32a
dainis-santars <dainis.santars@gmail.com>
2021-12-27 18:28:30 +0200

    Spēle sāk pēc kkā izskatīties :/

---
+++ md.js
@@ -0,0 +1,31 @@
+let vardi = ['abaks',
+'abate',
+'abats',
+'ābece',
+'ābele',
+'ābols',
+'acīte',
+'adata',
+'ādere',
+'afēra',
+'afiša',
+'agars']
+
+document.getElementById("demo").innerHTML = vardi
+
+const viens = vardi[Math.floor(Math.random()*vardi.length)]
+document.getElementById("viens").innerHTML = viens
+
+const sajaukts = viens.split("")
+document.getElementById("sajaukts").innerHTML = sajaukts
+
+const miniMani = sajaukts.sort((a, b) => 0.5 - Math.random())
+document.getElementById("miniMani").innerHTML = miniMani
+
+const miniManiVards = miniMani.join('')
+document.getElementById("miniManiVards").innerHTML = miniManiVards
+
+function fn1() {
+    var ievade = document.getElementById("meginajums").value;
+    document.getElementById("ievade").innerHTML = ievade;
+}
