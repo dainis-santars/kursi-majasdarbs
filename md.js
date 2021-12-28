aeff162df3638444a043790ac06b7ab80eade7cf
dainis-santars <dainis.santars@gmail.com>
2021-12-28 11:27:10 +0200

    Sariktēts pulkstenis un taimeris

--- md.js
+++ md.js
@@ -25,11 +25,6 @@ document.getElementById("miniMani").innerHTML = miniMani
 const miniManiVards = miniMani.join('')
 document.getElementById("miniManiVards").innerHTML = miniManiVards
 
-function fn1() {
-    var ievade = document.getElementById("meginajums").value;
-    document.getElementById("ievade").innerHTML = ievade;
-}
-
 function slepsana() {
     var x = document.getElementById("myDIV");
     if (x.style.display === "none") {
@@ -38,3 +33,52 @@ function slepsana() {
         x.style.display = "none";
     }
 }
+
+function fn1() {
+    var ievade = document.getElementById("meginajums").value;
+    document.getElementById("ievade").innerHTML = ievade;
+}
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + "." + m + "." + s;
+    document.getElementById("laiks").innerText = time;
+    document.getElementById("laiks").textContent = time;
+
+    setTimeout(showTime, 1000);
+
+}
+showTime();
+
+
+var minutes =0;
+var sekundes =0;
+function startTimer(){
+    let timex = setTimeout(function () {
+        sekundes++;
+        if (sekundes > 59) {
+            sekundes = 0;
+            minutes++;
+
+            if (minutes < 10) {
+                $("#minutes").text('0' + minutes + '.');
+            } else $("#minutes").text(minutes + '.');
+        }
+        if (sekundes < 10) {
+            $("#sekundes").text('0' + sekundes);
+        } else {
+            $("#sekundes").text(sekundes);
+        }
+
+        startTimer();
+    }, 1000);
+}
+
