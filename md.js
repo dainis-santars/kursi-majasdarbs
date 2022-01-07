e2ba26900b6ebf93d6aa3f2bbab239b39f39ca57
dainis-santars <dainis.santars@gmail.com>
2022-01-07 11:43:30 +0200

    Novākti no acīm visi starpstāvokļi

--- md.js
+++ md.js
@@ -2,16 +2,16 @@ let masivs = []
 
 jQuery.get('5burti.txt', function(data) {
     masivs = data.split('\n');
-    document.getElementById("dati").innerHTML = masivs
+    // document.getElementById("dati").innerHTML = masivs
 
     const viensVardsnoMasiva = masivs[Math.floor(Math.random()*masivs.length)]
-    document.getElementById("viensVardsnoMasiva").innerHTML = viensVardsnoMasiva
+    // document.getElementById("viensVardsnoMasiva").innerHTML = viensVardsnoMasiva
 
     const sadalitsPaBurtiem = viensVardsnoMasiva.split("")
-    document.getElementById("sadalitsPaBurtiem").innerHTML = sadalitsPaBurtiem
+    // document.getElementById("sadalitsPaBurtiem").innerHTML = sadalitsPaBurtiem
 
     const sajauktsVards = sadalitsPaBurtiem.sort((a, b) => 0.5 - Math.random())
-    document.getElementById("sajauktsVards").innerHTML = sajauktsVards
+    // document.getElementById("sajauktsVards").innerHTML = sajauktsVards
 
     const putra = sajauktsVards.join('')
     document.getElementById("putra").innerHTML = putra
@@ -32,26 +32,6 @@ function fn1() {
     var ievade = document.getElementById("meginajums").value;
     document.getElementById("ievade").innerHTML = ievade;
 }
-
-function showTime(){
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-
-    var time = h + "." + m + "." + s;
-    document.getElementById("laiks").innerText = time;
-    document.getElementById("laiks").textContent = time;
-
-    setTimeout(showTime, 1000);
-
-}
-showTime();
-
 
 var minutes =0;
 var sekundes =0;
