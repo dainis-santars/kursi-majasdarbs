828854c0df2b90fcb2b800c1eda921735ea0d76b
dainis-santars <dainis.santars@gmail.com>
2022-03-12 19:08:54 +0200

    Tagad var spaidīt pogas

--- md-pa-jaunam.js
+++ md-pa-jaunam.js
@@ -1,12 +1,4 @@
-/*
-ToDo:
-No sākuma iegūsti datus un tos apstrādā (pa burtiem dalīšana un tā tālāk).
-Tad iegūsti references uz visiem HTML elementiem, ar kuriem strādāsi.
-Tad tiem HTML elementiem uzliec sākotnējās vērtības (ja tā ir domāts) un arī funkcijas, kas nostrādās pie ievades notikumiem (oninput, onclick, ...).
-Un to visu tajā galvenajā asinhronajā funkcijā.
-starp citu, koda likšana kaut kādā galvenajā funkcijā ir ierasta prakse – tas rada scope un no ārpuses nekādi mainīgo vai funkciju nosaukumi nebūs redzami. Tas ir svarīgi, piemēram, lai kodu var dalīt pa moduļiem, kuros var izmantot vienus un tos pašus mainīgo un funkciju nosaukumus – tā, lai netraucē viens otram. Winking face
-*/
-
+let guessed = [];
 result = fetch('6burti.txt')
 result.then(function(response) {
     return response.text();
@@ -15,9 +7,11 @@ result.then(function(response) {
     masivs6 = dati.split('\n');
     console.log(masivs6)
     const viensVardsnoMasiva = masivs6[Math.floor(Math.random()*masivs6.length)]
+    console.log(viensVardsnoMasiva)
     document.getElementById("viensVardsnoMasiva").innerHTML = viensVardsnoMasiva
     const sadalitsPaBurtiem = viensVardsnoMasiva.split("")
     const sajauktsVards = sadalitsPaBurtiem.sort((a, b) => 0.5 - Math.random())
+    let wordStatus = null;
     let pogasHTML = sajauktsVards.map(letter =>
         `
       <button
@@ -30,57 +24,33 @@ result.then(function(response) {
     `).join('');
 
     document.getElementById('keyboard').innerHTML = pogasHTML;
+
     const putra = sajauktsVards.join('')
 
     document.getElementById("putra").innerHTML = putra
+
+    let answer = viensVardsnoMasiva
+    console.log(answer)
 });
 
-/* Laiks */
-var sek =0;
-function pad (val) { return val > 9 ? val : "0" + val; }
-setInterval(function (){
-    document.getElementById("sekundes").innerHTML=pad(++sek%60);
-    document.getElementById("minutes").innerHTML=pad(parseInt(sek/60,10));
-}, 1000);
+function guessedWord() {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
 
-/* Ilgums */
-function sakumaLaiks() {
-    let currentDate1 = new Date();
-    let time1 = currentDate1.getMinutes() + ":" + currentDate1.getSeconds();
-    console.log(time1)
-}
-function beiguLaiks(){
-    let currentDate2 = new Date();
-    let time2 = currentDate2.getMinutes() + ":" + currentDate2.getSeconds();
-    console.log(time2)
-    // let ilgums = time2 - (sakumaLaiks())
-    // console.log(ilgums)
-}
-var ilgums = beiguLaiks() - sakumaLaiks()
-document.getElementById("ilgums").innerHTML = ilgums
-
-/* lietotāja ievade */
-function panemtTekstu(){
-    let input = document.getElementById("ievade1").value;
-    // alert(input)
 }
 
-function changeColor(newColor) {
-    var elem = document.getElementById('viensVardsnoMasiva');
-    elem.style.color = newColor;
-}
-
-function irNavVienadi(){
-    var vert1 = document.getElementById('viensVardsnoMasiva').innerText;
-    var vert2 = document.getElementById("ievade1").value;
-    console.log(vert1);
-    console.log(vert2);
-    if (vert1 == vert2) {alert("sakrīt")}
-    else if (vert1 != vert2) {alert("nesakrīt")}
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute('disabled', true);
+    console.log(guessed)
+    document.getElementById('wordSpotlight').innerHTML = guessed.join('');
 }
 
 sakumaLaiks();
 panemtTekstu();
 irNavVienadi();
 beiguLaiks();
+
+guessedWord();
+handleGuess();
+
 uzblieztPogas();
